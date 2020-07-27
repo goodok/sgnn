9d9565c52e52e74daee77ebb7d840f5b8316a93a
Alexey U. Gudchenko <proga@goodok.ru>
2020-07-27 17:33:53 +0300

    Neptune logger

--- sgnn_pl_hydra/train.py
+++ sgnn_pl_hydra/train.py
@@ -11,7 +11,10 @@ import numpy as np
 import torch
 
 import pytorch_lightning as pl
+from pytorch_lightning.logging.neptune import NeptuneLogger
+
 from model_lightning import LightningTemplateModel
+from utils import dict_flatten
 
 SEED = 2334
 torch.manual_seed(SEED)
@@ -25,7 +28,26 @@ def main(hparams):
     """
     Main training routine specific for this project
     :param hparams:
+
     """
+
+    # 0 INIT TRACKER
+    # https://docs.neptune.ai/integrations/pytorch_lightning.html
+    neptune_params = hparams.tracker.neptune
+    if neptune_params.fn_token is not None:
+            with open(os.path.expanduser(neptune_params.fn_token), 'r') as f:
+                token = f.readline().splitlines()[0]
+                os.environ['NEPTUNE_API_TOKEN'] = token
+
+    hparams_flatten = dict_flatten(hparams, sep='.')
+    experiment_name = hparams.tracker.get('experiment_name', None)
+
+    neptune_logger = NeptuneLogger(
+        project_name=neptune_params.project_name,
+        experiment_name=experiment_name,
+        params=hparams_flatten,
+        )
+
     # ------------------------
     # 1 INIT LIGHTNING MODEL
     # ------------------------
@@ -42,6 +64,7 @@ def main(hparams):
         gpus=hparams.gpus,
         distributed_backend=hparams.distributed_backend,
         precision=16 if hparams.use_16bit else 32,
+        logger=neptune_logger,
     )
 
     # ------------------------
