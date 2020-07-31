2e85d10f6da01d2e1338a38bb90279c09b278d4a
Alexey U. Gudchenko <proga@goodok.ru>
2020-07-31 15:02:26 +0300

    tracker: add versions of numpy and plyfile

--- sgnn_pl_hydra/train.py
+++ sgnn_pl_hydra/train.py
@@ -68,7 +68,8 @@ def main(hparams):
 
     # log
     if tracker is not None:
-        watermark_s = watermark(packages=['python', 'nvidia', 'cudnn', 'hostname', 'torch', 'sparseconvnet', 'pytorch-lightning', 'hydra-core'])
+        watermark_s = watermark(packages=['python', 'nvidia', 'cudnn', 'hostname', 'torch', 'sparseconvnet', 'pytorch-lightning', 'hydra-core',
+                                          'numpy', 'plyfile'])
         log_text_as_artifact(tracker, watermark_s, "versions.txt")
         # arguments_of_script
         sysargs_s = str(sys.argv[1:])
